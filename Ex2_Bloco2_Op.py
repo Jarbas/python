de9ec0825e8d80d3a4a31cd27671303fb61aad39
Jarbas Coltro <jarbascltr@gmail.com>
2025-03-28 15:58:08 -0300

    UTFP - Ex Bloco1

---
+++ Ex2_Bloco2_Op.py
@@ -0,0 +1,11 @@
+x = 20
+x += 30
+print(x)
+x -= 10
+print(x)
+x /= 2
+print(x)
+x **= 2
+print(x)
+x %= 2
+print(x)
---
+++ Ex_Bloco1_Op.py
@@ -0,0 +1,6 @@
+y = -10.5
+print(abs(y))
+z = int(y)
+print(z)
+x = divmod(z, 2)
+print(x)
